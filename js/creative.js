8374c344e59db72604c25145764aa70d0ce5daed
chequebsx <gustavotorres@Gustavos-MacBook-Pro-3.local>
2017-11-10 18:00:38 +0530

    Create popups for events

--- js/creative.js
+++ js/creative.js
@@ -76,4 +76,9 @@
     type: 'iframe'
     // other options
   });
+
+  $('.open-popup-link').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
 })(jQuery); // End of use strict
